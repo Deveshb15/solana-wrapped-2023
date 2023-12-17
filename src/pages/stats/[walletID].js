88eef7ca1b978d698fd03db3e4679cc9a3eb2a45
Your Vidhatha <vtvidhathatallapally@gmail.com>
2023-12-17 17:59:23 +0530

    added ui screens

---
+++ src/pages/stats/[walletID].js
@@ -0,0 +1,104 @@
+import React, { useState,useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Card1 from '@/components/cards/Card1';
+import Card2 from '@/components/cards/Card2';
+import Card3 from '@/components/cards/Card3';
+import Card4 from '@/components/cards/Card4';
+import Card5 from '@/components/cards/Card5';
+import Card6 from '@/components/cards/Card6';
+import Card7 from '@/components/cards/Card7';
+import Card8 from '@/components/cards/Card8';
+
+import TopNav from '@/components/TopNav';
+import Loading from '@/components/loading';
+import Head from 'next/head'
+
+
+const Carousel = ({address}) => {
+    const [activeSlide, setActiveSlide] = useState(0);
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+    const { walletID } = router.query;
+  console.log('wallet id is', walletID)
+  
+
+    const goToSlide = (index) => {
+        setActiveSlide(index);
+    };
+
+    const goToPrevSlide = () => {
+        setActiveSlide(activeSlide - 1 >= 0 ? activeSlide - 1 : slides.length - 1);
+    };
+
+    const goToNextSlide = () => {
+        setActiveSlide(activeSlide + 1 < slides.length ? activeSlide + 1 : 0);
+    };
+
+//   useEffect(() => {
+//     console.log('inside data')
+//     fetch(`/api/data/${walletID}`)
+//       .then(response => response.json())
+//       .then(fetchedData => {
+//         setData(fetchedData);
+//         setLoading(false);
+
+    
+//       });
+//   }, []);
+
+
+//   if (loading) {
+//     return <Loading/>;
+//   }
+
+
+  const slides = [
+    <Card1/>, <Card2/>, <Card3 />,
+     <Card4 />, <Card5 />, <Card6 />, <Card7/> , <Card8/>]
+
+// const slides = [
+//     data && <Card1 />, 
+//     data?.highestTransaction?.[0] && <Card2 transactions={data?.highestTransaction[0]} />, 
+//     (data?.percentage && data?.arbitrumSent && data?.arbitrumReceived) && <Card3 percentage={data.percentage} sent={data.arbitrumSent} received={data.arbitrumReceived} />, 
+//     data?.nftCount && <Card4 nftCount={data.nftCount} />, 
+//     data?.nft && <Card5 nft={data.nft} />,
+//     (data?.transactions && data?.favouriteToken && data?.percentage && data?.nftCount && data?.nft) && <Card6 transactions={data.transactions} favToken={data.favouriteToken} percentage={data.percentage} nftCount={data.nftCount} nftData={data.nft} />
+//   ].filter(Boolean);
+
+
+    return (
+        <div className="min-h-screen p-8 bg-black relative"> 
+           <Head>
+      <title>Arbitrum Wrappd</title>
+    </Head>
+        <TopNav/>
+            <div className="md:w-64 mx-auto flex justify-center mt-12 mb-4">
+                {slides.map((_, index) => (
+                    <div key={index} className={`flex-1 h-2 w-5 rounded-full md:w-16 mx-1 ${index === activeSlide ? 'bg-green-500' : 'bg-dark-blue'}`}></div>
+                ))}
+            </div>  
+            <div className="md:w-96 mx-auto" style={{ scrollSnapType: 'x mandatory' }}>
+            {slides.map((Slide, index) => (
+                    <div key={index} className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all duration-300 ${index === activeSlide ? 'opacity-100 z-10' : 'opacity-0 z-0'}`}>
+                        {Slide}
+                    </div>
+                ))}
+                <div className="absolute top-1/2 left-0 right-0 flex justify-between px-12 md:px-48 z-20">
+                <button onClick={goToPrevSlide} className="cursor-pointer bg-gray rounded-full shadow-md p-2">
+                    <svg className="w-6 h-6 text-gray-800" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M15 19l-7-7 7-7"></path>
+                    </svg>
+                </button>
+                <button onClick={goToNextSlide} className="cursor-pointer bg-gray rounded-full shadow-md p-2">
+                    <svg className="w-6 h-6 text-gray-800" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                    <path d="M9 5l7 7-7 7"></path>
+                    </svg>
+                </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        heading: ['Space Grotesk', 'sans-serif'],
+        dm: ['DM Sans', 'sans-serif'],
+        mono: ['Monomaniac One', 'monospace'],
+      },
+      colors: {
+        "formfield": '#1E2228', 
+        "dark-blue": "#1E2228",
+        "neutral-blue": "#21252C",
+        "light-blue": "#252B35",
+        "rose-pink": "#E25376",
+        "gray":"#1E1E1E",
+        "black":"#131313",
+        "ash":"#7B7B7B",
+        "fade":'#CCC'
+        
+      
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -16,3 +34,4 @@ module.exports = {
   },
   plugins: [],
 }
+
